ca2d893a7c78399fae48f06461057bd99cfbdb13
Lry722 <958515687@qq.com>
2024-02-14 15:17:36 +0800

    init

---
+++ bitarray.h
@@ -0,0 +1,186 @@
+#pragma once
+#include <array>
+#include <cstdint>
+#include <concepts>
+
+template <std::size_t ElementSize, std::size_t ArraySize, typename T = std::uint32_t>
+    requires std::is_unsigned_v<T>
+class BitArray
+{
+public:
+    static inline const std::size_t kArraySize = ArraySize;
+
+    class Access
+    {
+        BitArray<ElementSize, ArraySize, T> &data_;
+        std::size_t index_;
+
+    public:
+        Access(BitArray &data, std::size_t index) : data_(data), index_(index) {}
+        Access &operator=(const T value)
+        {
+            data_.set(index_, value);
+            return *this;
+        }
+        operator T() const
+        {
+            return data_.get(index_);
+        }
+    };
+    friend class Access;
+
+    class iterator
+    {
+    public:
+        using iterator_category = std::random_access_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = T;
+        using reference = const T &;
+
+        iterator(BitArray<ElementSize, ArraySize, T> *array, std::size_t index)
+            : array_(array), index_(index) {}
+
+        T operator*() const { return (*array_)[index_]; }
+
+        iterator &operator++()
+        {
+            ++index_;
+            return *this;
+        }
+        iterator operator++(int)
+        {
+            iterator tmp(*this);
+            operator++();
+            return tmp;
+        }
+
+        iterator &operator--()
+        {
+            --index_;
+            return *this;
+        }
+        iterator operator--(int)
+        {
+            iterator tmp(*this);
+            operator--();
+            return tmp;
+        }
+
+        iterator &operator+=(difference_type n)
+        {
+            index_ += n;
+            return *this;
+        }
+        iterator &operator-=(difference_type n)
+        {
+            index_ -= n;
+            return *this;
+        }
+
+        friend iterator operator+(iterator it, difference_type n) { return it += n; }
+        friend iterator operator+(difference_type n, iterator it) { return it += n; }
+        friend iterator operator-(iterator it, difference_type n) { return it -= n; }
+
+        friend difference_type operator-(iterator lhs, iterator rhs) { return lhs.index_ - rhs.index_; }
+
+        bool operator==(const iterator &other) const { return array_ == other.array_ && index_ == other.index_; }
+        bool operator<(const iterator &other) const { return index_ < other.index_; }
+
+    private:
+        BitArray<ElementSize, ArraySize, T> *array_;
+        std::size_t index_;
+    };
+
+    class const_iterator
+    {
+    public:
+        using iterator_category = std::random_access_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = T;
+        using reference = Access;
+
+        const_iterator(const BitArray<ElementSize, ArraySize, T> *array, std::size_t index)
+            : array_(array), index_(index) {}
+
+        reference operator*() const { return (*array_)[index_]; }
+
+        const_iterator &operator++()
+        {
+            ++index_;
+            return *this;
+        }
+        const_iterator operator++(int)
+        {
+            const_iterator tmp(*this);
+            operator++();
+            return tmp;
+        }
+
+        const_iterator &operator--()
+        {
+            --index_;
+            return *this;
+        }
+        const_iterator operator--(int)
+        {
+            const_iterator tmp(*this);
+            operator--();
+            return tmp;
+        }
+
+        const_iterator &operator+=(difference_type n)
+        {
+            index_ += n;
+            return *this;
+        }
+        const_iterator &operator-=(difference_type n)
+        {
+            index_ -= n;
+            return *this;
+        }
+
+        friend const_iterator operator+(const_iterator it, difference_type n) { return it += n; }
+        friend const_iterator operator+(difference_type n, const_iterator it) { return it += n; }
+        friend const_iterator operator-(const_iterator it, difference_type n) { return it -= n; }
+
+        friend difference_type operator-(const_iterator lhs, const_iterator rhs) { return lhs.index_ - rhs.index_; }
+
+        bool operator==(const const_iterator &other) const { return array_ == other.array_ && index_ == other.index_; }
+        bool operator<(const const_iterator &other) const { return index_ < other.index_; }
+
+    private:
+        const BitArray<ElementSize, ArraySize, T> *array_;
+        std::size_t index_;
+    };
+
+    typedef T value_type;
+    typedef Access reference;
+
+public:
+    std::size_t size() { return kArraySize; }
+    bool empty() { return false; }
+
+    T get(std::size_t index);
+    void set(std::size_t index, const T value);
+
+    Access operator[](std::size_t index) { return Access(*this, index); }
+    T operator[](std::size_t index) const { return get(index); }
+
+    typename BitArray<ElementSize, ArraySize, T>::iterator begin() { return iterator(this, 0); }
+    typename BitArray<ElementSize, ArraySize, T>::iterator end() { return iterator(this, kArraySize); }
+    typename BitArray<ElementSize, ArraySize, T>::const_iterator cbegin() const { return const_iterator(this, 0); }
+    typename BitArray<ElementSize, ArraySize, T>::const_iterator cend() const { return const_iterator(this, kArraySize); }
+
+private:
+    // 当UnitSize为32时，该bitarray最多支持64位的ElementSize
+    // 若UnitSize为16，则最多支持32位ElementSize，以此类推
+    static inline const std::size_t kElementSize = ElementSize;
+    static inline const std::size_t kUnitSize = sizeof(T);
+    static inline const std::size_t kInnerArraySize = (kElementSize * kArraySize + kUnitSize - 1) / kUnitSize;
+    // kMask 参与的表达式都会自动转为ull，并在最后转为T时截断高位不需要的内容
+    static inline const unsigned long long kMask = (1 << kElementSize) - 1;
+
+    std::array<T, kInnerArraySize> data{};
+};
+
+#include "bitarray.inl"
---
+++ bitarray.inl
@@ -0,0 +1,44 @@
+#pragma once
+#include "bitarray.h"
+#include <stdexcept>
+#include <tuple>
+#include <format>
+
+auto calcIndexInfo(const std::size_t index, const std::size_t element_size, const std::size_t unit_size)
+{
+    return std::tuple{(index * element_size) / unit_size, (index * element_size) % unit_size};
+}
+
+template <std::size_t ElementSize, std::size_t ArraySize, typename T>
+    requires std::is_unsigned_v<T>
+inline T BitArray<ElementSize, ArraySize, T>::get(std::size_t index)
+{
+    if (index >= kArraySize)
+        throw std::out_of_range(std::format("BitArray: index {} out of range!", index));
+
+    const auto [kIndexInData, kIndexInUnit] = calcIndexInfo(index, kElementSize, kUnitSize);
+
+    // 读取当前unit中的位
+    T result{static_cast<T>((data[kIndexInData] >> kIndexInUnit) & kMask)};
+    if (kUnitSize - kIndexInUnit < kElementSize)
+        // 读取超出当前unit的位
+        result |= ((data[kIndexInData + 1] << kIndexInUnit) & kMask);
+
+    return result;
+}
+
+template <std::size_t ElementSize, std::size_t ArraySize, typename T>
+    requires std::is_unsigned_v<T>
+inline void BitArray<ElementSize, ArraySize, T>::set(std::size_t index, const T value)
+{
+    if (index >= kArraySize)
+        throw std::out_of_range(std::format("BitArray: index {} out of range!", index));
+
+    const auto [kIndexInData, kIndexInUnit] = calcIndexInfo(index, kElementSize, kUnitSize);
+
+    // 写入当前unit中的位
+    data[kIndexInData] = data[kIndexInData] & ~(kMask << kIndexInUnit) | (value << kIndexInUnit);
+    if (kUnitSize - kIndexInUnit < kElementSize)
+        // 写入超出当前unit的位
+        data[kIndexInData + 1] = data[kIndexInData + 1] & ~(kMask >> (kUnitSize - kIndexInUnit)) | (value >> kIndexInUnit);
+}
